c5d45f3c3543327fd723e2a1661ed282999653c8
banirasulia <bani.rasulia@gmail.com>
2021-09-18 18:25:55 +0700

    tugas pertama

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+{
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Bani Rasulia 
+            saya mahasiswa UBSI
+            Jurusan Sistem Informasi Semester 3 
+            Tempat Tinggal saya di ciomas Kabupaten Bogor
+            Saya senang membaca, mendengarkan musik dan menonton film"; 
+    } 
+}
