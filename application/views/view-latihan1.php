d4eb375fd8703e196ad46a9a9c74a94f87e65d60
banirasulia <bani.rasulia@gmail.com>
2021-09-25 17:27:06 +0700

    tugas kedua

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan 1</title>
+</head>
+<body>
+	Halo Kawan..yuk kita belajar Web Programming..!!!<br>
+	Nilai 1 = <?= $nilai1; ?> <br>
+	Nilai 2 = <?= $nilai2; ?> <br>
+	ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " .
+		$nilai2 . " = " . $hasil; ?>
+
+</body>
+</html>
---
+++ bcit-ci-CodeIgniter-b73eb19/application/controllers/latihan1.php
@@ -0,0 +1,8 @@
+ <?php
+ class latihan1 extends CI_Controller
+ {
+ 	Public function index()
+ 	{
+ 		echo "SELAMAT DATANG MAASISWA /MAASISWI .......SELAMAT BERGABUNG DI KELAS WEB PROGRAMMING II......";
+ 	}
+ }
